2721166aba301d9bac7c8c9c190ce690b85707aa
Your Name <you@example.com>
2021-03-07 12:08:24 -0500

    Finished

--- app/views.py
+++ app/views.py
@@ -28,6 +28,18 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('logged out.', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    return render_template('secure-page.html')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -37,9 +49,9 @@ def login():
         # and not just one field
         if form.validate_on_submit():
             # Get the username and password values from the form.
-            name = form.username.data
+            username = form.username.data
             password = form.password.data
-            user = UserProfile.filter_by(username=username).first()
+            user = UserProfile.query.filter_by(username=username).first()
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
@@ -48,11 +60,12 @@ def login():
             if user is not None and check_password_hash(user.password, password):
 
             # get user id, load into session
+                flash('Logged in successfully','success')
                 login_user(user)
-                flash("Logged in successfully")
-
             # remember to flash a message to the user
-            return redirect(url_for("secure-page"))  # they should be redirected to a secure-page route instead
+                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
+            else:
+                flash('Username or Password is incorrect.', 'danger')
     return render_template("login.html", form=form)
 
 
